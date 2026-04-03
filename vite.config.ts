e7598a29369879b4aac8603fe6a39199be1934f9
unknown <nurbekrasulov71@gmail.com>
2026-04-03 09:44:22 +0500

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@zxing/browser': path.resolve(__dirname, 'node_modules/@zxing/browser/esm/index.js'),
+      '@zxing/library': path.resolve(__dirname, 'node_modules/@zxing/library/esm/index.js'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
